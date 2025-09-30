c2149fe98634ad6ccfe5464099e66e46d2dd2190
litoninot <litoninot12345@gmail.com>
2025-09-30 21:33:18 +0700

    done

--- Panda And Openpyxl/panda1.py
+++ Panda And Openpyxl/panda1.py
@@ -17,5 +17,40 @@ book = pd.read_excel("Book1.xlsx")
 
 # print(book.iloc[1:2])
 
-for index,row in book.iterrows():
-    print(index,row)
+# for index,row in book.iterrows():
+    # print(index,row["Nama"])
+
+# book.loc[df["Nama"] == "Pepew"]
+    # IN 
+    # NOT IN 
+    # AND 
+    # OR 
+
+# print(book.describe())
+
+# book.sort_values("Nama",ascending=False)
+# book.sort_values(["Nama","Nilai"],ascending=False)
+# book.sort_values(["Nama","Nilai"],ascending=[1,0])
+
+# book["Total"] = book["Nilai"] + book["Nilai Lain"]
+
+# book = book.drop(columns=['Total'])
+
+# book['Total'] = df.iloc[:,4:10].sum(axios=1)
+
+# cols = list(book.columns)
+# book = book[cols[0:4] + [cols[-1]] + cols[4:12]]
+
+# book.to_csv("halo.csv",index=False)
+# to_excel
+
+# halo = df.iloc[df["Nama"] == "aku"]
+# halo.to_csv("halo.csv")
+
+# book.loc[df["Nama"].str.contains("Mega")]
+
+# book.loc[dg["Nama"] === 'A',"Nama"] = "Agus"
+
+# book.groupby(["Nama"])
+
+# book.groupby(["Nama"]).mean().sort_values("Nilai",ascending=False)
